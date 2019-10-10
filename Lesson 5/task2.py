62ef67fba2c13509651942edbb31fd22cf82f6c4
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2019-10-10 14:14:11 +0300

    Update task2.py
    решена 2я задача

--- Lesson 5/task2.py
+++ Lesson 5/task2.py
@@ -1,4 +1,10 @@
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+# os.listdir(path=".") - список файлов и директорий в папке.
 
-#os.listdir(path=".") - список файлов и директорий в папке.
+import sys
+import os
+
+dir_path = os.path.join(os.getcwd())
+os.listdir(dir_path)
+print('Папки в директории\n', os.listdir(dir_path))
