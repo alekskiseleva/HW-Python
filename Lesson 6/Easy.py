d5e9eedbc9e3db8bdbd62f8749128ff1d9cdbab5
alekskiseleva <aleks.kiseleva@gmail.com>
2019-10-14 13:17:11 +0300

    update task Easy (1\2)

--- Lesson 6/Easy.py
+++ Lesson 6/Easy.py
@@ -12,49 +12,40 @@ import math
 
 class Triangles:
    # задаем атрибуты
-   #координаты точек
-    # a b c
-    # p #полупериметр
-    # h #высота
-    # per #периметр
-    # Sq #площадь
-    # h #высота треугольника
-
-    # задаем методы
-
+   x1, x2, x3, y1, y2, y3 = 0, 0, 0, 0, 0, 0
+   a, b, c, p, h, per = 0, 0, 0, 0, 0, 0
+    # координаты точек
     # Задаем координаты
 
-    def coordinates_triangles(self):
-        x1, y1 = list(map(int, input('Введите координаты точки A(x1,y1): ').split()))
-        x2, y2 = list(map(int, input('Введите координаты точки B(x2,y2): ').split()))
-        x3, y3 = list(map(int, input('Введите координаты точки C(x3,y3): ').split()))  # ввод координат точек
-        print(x1, y1)
-        print(x2, y2)
-        print(x3, y3)
-        return x1, x2, x3, y1, y2, y3
+   def __init__(self):
+       # координаты точек
+       self.x1, self.y1 = list(map(int, input('Введите координаты точки A(x1,y1): ').split()))
+       self.x2, self.y2 = list(map(int, input('Введите координаты точки B(x2,y2): ').split()))
+       self.x3, self.y3 = list(map(int, input('Введите координаты точки C(x3,y3): ').split()))
 
     # вычисление сторон через векторы
     def tr_length(self):
-        a = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-        b = math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2)
-        c = math.sqrt((x3 - xx) ** 2 + (y3 - y1) ** 2)
-        print('Длины сторон треугольника', a, b, c)
+        self.a = math.sqrt((self.x2 - self.x1) ** 2 + (self.y2 - self.y1) ** 2)
+        self. b = math.sqrt((self.x3 - self.x2) ** 2 + (self.y3 - self.y2) ** 2)
+        self.c = math.sqrt((self.x3 - self.x1) ** 2 + (self.y3 - self.y1) ** 2)
+        print('Длины сторон треугольника', self.a, self.b, self.c)
+        return self.a, self.b, self.c
 
     # Вычисление периметра
     def tr_perimeter(self):
-        per = a + b + c
-        print('Периметр треугольника', per)
+        self.per = self.a + self.b + self.c
+        print('Периметр треугольника', self.per)
 
     # Вычисление высоты
     def tr_height(self):
-        p = (1 / 2) * per
-        h = (2 / a) * (math.sqrt(p * (p - a) * (p - b) * (p - c)))
-        print('Высота треугольника: ', p, h)
-        # Вычисление площади
+        self.p = (1 / 2) * self.per
+        self.h = (2 / self.a) * (math.sqrt(self.p * (self.p - self.a) * (self.p - self.b) * (self.p - self.c)))
+        print('Высота треугольника: ', self.p, self.h)
 
+    # Вычисление площади
     def tr_square(self):
-        Sq = (1 / 2) * a * h
-        print('Площадь треугольника: ', Sq)
+        self.Sq = (1 / 2) * self.a * self.h
+        print('Площадь треугольника: ', self.Sq)
 
 
 class Trapezes:
@@ -73,7 +64,7 @@ class Trapezes:
 
 
 trtr = Triangles()
-trtr.coordinates_triangles()
+
 trtr.tr_length()
 trtr.tr_perimeter()
 trtr.tr_square()
