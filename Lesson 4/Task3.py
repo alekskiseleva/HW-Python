9aa807733caa585710599350ae5108237587c9d9
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2019-11-13 18:53:17 +0300

    Update Task3.py
    выполнена часть задачи

--- Lesson 4/Task3.py
+++ Lesson 4/Task3.py
@@ -5,4 +5,14 @@
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
 
+from random import randint
 
+m = 2500
+
+my_list = ''
+
+for el in range(m):
+    x = str(randint(0, 9))
+    my_list = my_list + x
+
+print(my_list)
