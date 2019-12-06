45d9027b5f222bd106018904232e19cd27693500
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2019-12-06 14:21:48 +0300

    Update task1.py

--- lesson 3/task1.py
+++ lesson 3/task1.py
@@ -7,21 +7,27 @@ __author__ = 'Киселева Александра'
 
 def fibonacci(n, m):
     fib1 = fib2 = 1
+    fib_list = []
 
     if (n < 2) or (m < 2):
+        print('При заданных числах нельзя вывести ряд')
         quit()
 
     else:
+
         for i in range(2, n):
             fib1, fib2 = fib2, fib1 + fib2
 
         for j in range(m - n + 1):
             fib1, fib2 = fib2, fib1 + fib2
-            print(fib2, end=' ')
+            fib_list.append(fib2)
+
+    return fib_list
 
 
 n = int(input("Номер n элемента ряда Фибоначчи: "))
 m = int(input("Номер m элемента ряда Фибоначчи: "))
 
-print('\nРяд чисел Фибоначчи с n =', n, 'по m =', m, '\n')
 fibonacci(n, m)
+for i in fib_list:
+    print(i, end=' ')
